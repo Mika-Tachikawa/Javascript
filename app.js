ac3248abd032771e8da6756da4e2bc10847b8549
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-05-10 09:32:02 +0000

    Initial Commit1

--- app.js
+++ app.js
@@ -2,9 +2,6 @@
   //省略形　本当は(document).ready(function () {
 $(function(){
   $('.box1').on('click', function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+    $(this).slideUp();
   });
 });
